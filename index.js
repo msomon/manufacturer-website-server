e3cdbaa1d9c5b78536872ca1f21f415699bcd405
msomon <hsomon940@gmail.com>
2022-05-22 22:54:09 +0600

    add get

--- index.js
+++ index.js
@@ -2,13 +2,42 @@ const express = require("express");
 const cors = require('cors')
 const app =express()
 const port = process.env.PORT || 5000 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const { query } = require("express");
 require('dotenv').config();
 
 
 //middelwere  //
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.13hjh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+ await client.connect()
+ const toolsCollection = client.db('electronicsManufacturer').collection('tools')
+
+app.get('/tools',async(req,res)=>{
+  
+
+  const result = await toolsCollection.find({}).toArray()
+res.send(result)
+
+})
+
+
+
+  
+}
+
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
