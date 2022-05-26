365bfb1028db6d2c516668607e0b4afeecb2fb2e
msomon <hsomon940@gmail.com>
2022-05-26 08:09:14 +0600

    add admin

--- index.js
+++ index.js
@@ -92,6 +92,12 @@ app.put('/users/:email', async (req, res) => {
   const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
   res.send({ result, token });
 })
+app.get('/admin/:email', async(req, res) =>{
+  const email = req.params.email;
+  const user = await usersCollection.findOne({email: email});
+  const isAdmin = user.role === 'admin';
+  res.send({admin: isAdmin})
+})
 
 app.put('/users/admin/:email', async (req ,res) => {
   const email = req.params.email;
